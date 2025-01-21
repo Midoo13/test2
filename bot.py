bb326fd859a7ad72d2d290504ae1050657cd4e01
Midoo13 <167088079+Midoo13@users.noreply.github.com>
2025-01-21 08:47:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ keep_alive()  # This will keep the bot alive
 
 API_TOKEN = '7424829556:AAEarbNLH2Or_XD8P83xwI6qOiDj2zIuTOU'
 GIST_URL = 'https://api.github.com/gists/981d16aec654291008480155fb7eee53'
-GIST_TOKEN = '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'
+GIST_TOKEN = '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'
 
 bot = telebot.TeleBot(API_TOKEN)
 
